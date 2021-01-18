fa880af8f544272cbf203637a17107949bf3df26
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-18 06:52:54 +0300

    connect template library and add a function to post and display pics

--- insta/templatetags/pic.py
+++ insta/templatetags/pic.py
@@ -8,11 +8,11 @@ User = get_user_model()
 
 register = template.Library()
 
-# @register.simple_tag
-# def get_pic(username):
-#     user = User.objects.filter(username = username).first()
-#     profile = Profile.objects.filter(user = user).exists()
-#     if profile:
-#         return user.profile.profile_image.url
-#     else:
-#         return None
+@register.simple_tag
+def get_pic(username):
+    user = User.objects.filter(username = username).first()
+    profile = Profile.objects.filter(user = user).exists()
+    if profile:
+        return user.profile.profile_image.url
+    else:
+        return None
