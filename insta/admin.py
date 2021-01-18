889f55392d53668ee6a3435fd5375e15f0113fc6
sheillanjoroge <sheillan.njoroge@gmail.com>
2021-01-18 06:54:42 +0300

    add admin function

--- insta/admin.py
+++ insta/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Profile, Post, Follower, Like, Comment
 
-# admin.site.register(Profile)
-# admin.site.register(Post)
-# admin.site.register(Follower)
-# admin.site.register(Like)
-# admin.site.register(Comment)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Follower)
+admin.site.register(Like)
+admin.site.register(Comment)
